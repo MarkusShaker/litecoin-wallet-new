7551553066929f76de93fded605e1775a0ae6054
MarkusShaker <markus.shaker93@gmail.com>
2016-06-09 23:25:36 +0300

    Changed string values from Litecoin to Colchestecoin

--- wallet/src/de/schildbach/colchestercoinwallet/Constants.java
+++ wallet/src/de/schildbach/colchestercoinwallet/Constants.java
@@ -37,35 +37,35 @@ public class Constants
 	public static final NetworkParameters NETWORK_PARAMETERS = TEST ? TestNet3Params.get() : MainNetParams.get();
 	private static final String FILENAME_NETWORK_SUFFIX = NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_MAINNET) ? "" : "-testnet";
 
-	public static final String WALLET_FILENAME = "litecoinwallet" + FILENAME_NETWORK_SUFFIX;
+	public static final String WALLET_FILENAME = "colchestercoinwallet" + FILENAME_NETWORK_SUFFIX;
 
-	public static final String WALLET_FILENAME_PROTOBUF = "litecoinwallet-protobuf" + FILENAME_NETWORK_SUFFIX;
+	public static final String WALLET_FILENAME_PROTOBUF = "colchestercoinwallet-protobuf" + FILENAME_NETWORK_SUFFIX;
 
 	public static final String WALLET_KEY_BACKUP_BASE58 = "key-backup-base58" + FILENAME_NETWORK_SUFFIX;
 
 	public static final File EXTERNAL_WALLET_BACKUP_DIR = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-	public static final String EXTERNAL_WALLET_KEY_BACKUP = "litecoinwallet-keys" + FILENAME_NETWORK_SUFFIX;
+	public static final String EXTERNAL_WALLET_KEY_BACKUP = "colchestercoinwallet-keys" + FILENAME_NETWORK_SUFFIX;
 
 	public static final String BLOCKCHAIN_FILENAME = "blockchain" + FILENAME_NETWORK_SUFFIX;
 
 	public static final String CHECKPOINTS_FILENAME = "checkpoints" + FILENAME_NETWORK_SUFFIX;
 
-	private static final String BLOCKEXPLORER_BASE_URL_PROD = "http://explorer.litecoin.net/";
-	private static final String BLOCKEXPLORER_BASE_URL_TEST = "http://explorer.litecoin.net/";
+	private static final String BLOCKEXPLORER_BASE_URL_PROD = "http://explorer.colchestercoin.net/";
+	private static final String BLOCKEXPLORER_BASE_URL_TEST = "http://explorer.colchestercoin.net/";
 	public static final String BLOCKEXPLORER_BASE_URL = NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_MAINNET) ? BLOCKEXPLORER_BASE_URL_PROD
 			: BLOCKEXPLORER_BASE_URL_TEST;
 
 	public static final String MIMETYPE_TRANSACTION = "application/x-btctx";
 
 	public static final int MAX_NUM_CONFIRMATIONS = 7;
-	public static final String USER_AGENT = "Litecoin Wallet";
+	public static final String USER_AGENT = "Colchestercoin Wallet";
 	public static final String DEFAULT_EXCHANGE_CURRENCY = "USD";
 	public static final int WALLET_OPERATION_STACK_SIZE = 256 * 1024;
 	public static final long BLOCKCHAIN_STATE_BROADCAST_THROTTLE_MS = DateUtils.SECOND_IN_MILLIS;
 	public static final long BLOCKCHAIN_UPTODATE_THRESHOLD_MS = DateUtils.HOUR_IN_MILLIS;
 
-	public static final String CURRENCY_CODE_BTC = "LTC";
-	public static final String CURRENCY_CODE_MBTC = "mLTC";
+	public static final String CURRENCY_CODE_BTC = "CCh";
+	public static final String CURRENCY_CODE_MBTC = "mCCh";
 	public static final char CHAR_HAIR_SPACE = '\u200a';
 	public static final char CHAR_THIN_SPACE = '\u2009';
 	public static final char CHAR_ALMOST_EQUAL_TO = '\u2248';
